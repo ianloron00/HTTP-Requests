861e5fa27e470d7cb2ef7964965120fc9eb21d32
Ian Loron de Almeida <i198933@dac.unicamp.br>
2023-09-23 10:15:23 -0300

    Fetch data

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,32 @@
 import './App.css'
+import { useState, useEffect } from "react"
+const url = "http://localhost:3000/products"
 
 function App() {
+  const [products, setProducts] = useState([])
+
+  // resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url)
+      const data = await res.json()
+      setProducts(data)
+    }
+    fetchData()
+  }, [])
+
+  console.log(products)
+
   return (
     <>
+      <h1>Lista de Items</h1>
+      <ul>
+        {products.map((product) => (
+            <li key={product.id}>
+              {product.name} - R$: {product.price}
+            </li>
+        ))}
+      </ul>
     </>
   )
 }
